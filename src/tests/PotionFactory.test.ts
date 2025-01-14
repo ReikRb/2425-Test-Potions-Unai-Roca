c960304f836f093b92e937762a763f8e2ea59ad9
Unai <reikrb@gmail.com>
2025-01-14 09:06:00 +0100

    update - mocks

--- src/tests/PotionFactory.test.ts
+++ src/tests/PotionFactory.test.ts
@@ -1,1 +1,24 @@
+import { PotionFactory } from "../PotionFactory";
 import { DISEASES, INGREDIENTS } from "./mocks";
+
+describe('Cuando todos los ingredientes llevan el efecto "Restore".', () => {
+    it('El nombre deberá ser el correspondiente. Antidote of + "nombre enfermedad".', async () => {
+      PotionFactory.createPotion
+      const mockIngredients = [ ];
+      "lesser_restore_hit_points",
+      "lesser_restore_constitution"
+      const mockSetCurrentAttributes = jest.fn();
+  
+      
+  
+    //   expect(mockSetCurrentAttributes).toHaveBeenCalledWith({
+    //     charisma: 102,
+    //     constitution: 240,
+    //     dexterity: 174,
+    //     insanity: 117,
+    //     intelligence: 142,
+    //     strength: 253,
+    //   });
+    
+    });
+  });
